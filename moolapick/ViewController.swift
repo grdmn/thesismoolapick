61f72226d0b373c3e2891032727afd97c382fcde
grdmn <groragrod.mn@hotmail.com>
2016-12-11 20:13:18 +0700

    Initial Commit

---
+++ moolapick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moolapick
+//
+//  Created by Apple Macintosh on 12/11/16.
+//  Copyright © 2016 Apple Macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
